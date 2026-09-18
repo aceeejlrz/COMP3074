e8f126e13545977cbf46cbd639157b6a19c5b202
aceeejlrz <jzrl.ace.dc@gmail.com>
2026-09-18 15:47:25 -0400

    Lab 1: Introduction to Android App Development
    Empty Views Activity project with a TextView and Button. Pressing
    the button changes the label text to "Button clicked!".

---
+++ app/src/main/java/ca/gbc/comp3074/jdc/labex1/MainActivity.kt
@@ -0,0 +1,29 @@
+package ca.gbc.comp3074.jdc.labex1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val message = findViewById<TextView>(R.id.textViewMessage)
+        val button = findViewById<Button>(R.id.buttonChangeText)
+
+        button.setOnClickListener {
+            message.text = getString(R.string.button_clicked)
+        }
+    }
+}
